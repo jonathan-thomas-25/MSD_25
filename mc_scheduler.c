cc7c86f65841d31ea93dd790378c5e754ecc0998
jonathan-thomas-25 <jonthom@pdx.edu>
2023-11-07 15:10:24 -0800

    Skeletal codes and files added

---
+++ mc_scheduler.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main(int argc,char* argv[]){
+
+///////////////////////////////////VARIABLE DECLARATIONS////////////////////////////////
+FILE* file;
+char input_file[];
+char data[50];
+
+////////////////////////////////////////////////////////////////////////////////////////
+input_file=argv[1];
+file=fopen(input_file,"r");
+
+if(file==NULL){
+perror("[ERROR] The Trace file is empty");
+return(1);
+}
+
+else {
+
+while(fgets(data,50,file)!=NULL){
+
+printf("%s\n", data);
+
+}
+}
+fclose(file);
+
+}
+
+
+

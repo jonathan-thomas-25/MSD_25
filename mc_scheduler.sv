dd842963e2a73c0cb5d254918419a67bd1ea9de9
jonathan-thomas-25 <jonthom@pdx.edu>
2023-11-18 07:41:07 -0800

    added clock logic

--- mc_scheduler.sv
+++ mc_scheduler.sv
@@ -1,24 +1,24 @@
-module mc_scheduler;
+module nov17_mc;
 
-
-
-initial begin
+`timescale 1ps/1ps
 
 //////////////////////////VARIABLE DECLARATIONS//////////////
+
+
+//PARSING SIGNALS
 
 string filename;
 string filename1;
 int fh;
 int fh1;
 string line;
-
+int c=0;
 
 string clock_cycle;
 string core;
 string request_type;
 string address;
 string parts[4];
-
 
 logic [33:0] address_hex;
 logic [1:0] Byte_Select;
@@ -31,12 +31,19 @@ logic [15:0] row;
 logic channel;
 int clock_cycle_output;
 string command;
+int clock_cycle_int;
 
+//HARDWARE SIGNALS
+string main_q[$:15];
+bit [0:0] cpu_clk;
+always #104 cpu_clk=~cpu_clk;
+longint cpu_clk_count=0;
+bit [0:0] debug_check;
+bit [0:0] enable;
+//////////////////////ALWAYS LOGIC//////////////////////////
 
-
-
-////////////////////////////////////////////////////////////
-
+always @(posedge cpu_clk)begin :my_always_block
+if(cpu_clk_count==0)begin
 
 if($value$plusargs("INPUT_FILE=%s", filename))begin
 
@@ -45,8 +52,6 @@ $display("The filename is %s",filename);
 end
 
 else filename = "trace.txt";
-
-
 
 if($value$plusargs("OUTPUT_FILE=%s", filename1))begin
 
@@ -57,7 +62,6 @@ end
 else filename1 = "dram.txt";
 
 
-
 fh=$fopen(filename,"r");
 if(fh) $display("The trace file %s is successfully opened",filename);
 else $display("[ERROR] - Failed to open the trace file");
@@ -65,9 +69,17 @@ else $display("[ERROR] - Failed to open the trace file");
 fh1=$fopen(filename1,"w");
 
 
-while(!$feof(fh))begin
-$fgets(line,fh);
-//line.split(" ",parts);
+`ifdef DEBUG 
+debug_check=1'b1;
+`endif
+`ifndef DEBUG
+debug_check=1'b0;
+`endif
+
+end
+
+if(!$feof(fh) && $fgets(line,fh))begin
+
 
 $sscanf(line,"%s %s %s %s",parts[0],parts[1],parts[2],parts[3]);
 
@@ -76,21 +88,22 @@ core=parts[1];
 request_type=parts[2];
 address=parts[3];
 
-address_hex=address.atohex();
-clock_cycle_output=$urandom_range(50,100);
-//$display("The address is hex value is %h\n",address_hex);
+if(debug_check==1'b1)begin 
 
-channel=address_hex[6];
-Byte_Select=address_hex[1:0];
-col_low=address_hex[5:2];
-col_high=address_hex[17:12];
-col={col_high,col_low};
-ba=address_hex[11:10];
-bg=address_hex[9:7];
-row=address_hex[33:18];
+$display("###################### TRANSACTIONS ##############################\n");
+$display("clock cycle=%s\n",clock_cycle);
+$display("core number = %s\n",core);
+$display("request type=%s\n",request_type);
+$display("address=%s\n",address);
 
-/////////////////////DEBUG CODE//////////////////////////
-`ifdef DEBUG 
+c=c+1;
+if(c>=5)begin
+break;
+end
+
+end
+
+if(debug_check==1'b0)begin
 
 $fwrite(fh1,"###################### TRANSACTIONS ##############################\n");
 $fwrite(fh1,"clock cycle=%s\n",clock_cycle);
@@ -98,30 +111,31 @@ $fwrite(fh1,"core number = %s\n",core);
 $fwrite(fh1,"request type=%s\n",request_type);
 $fwrite(fh1,"address=%s\n",address);
 
-//if(request_type=="0" || request_type=="2")begin
-//$fwrite(fh1,"100  %d  PRE  %d  %d\n",channel,bg,ba);
-//$fwrite(fh1,"120  %d  ACT  %d  %d  %h\n",channel,bg,ba,row);
-//$fwrite(fh1, "128 %d  RD   %d  %d  %h\n",channel,bg,ba,col);
-//end
-
-//if(request_type=="1")begin
-//$fwrite(fh1,"100  %d  PRE  %d  %d\n",channel,bg,ba);
-//$fwrite(fh1,"120  %d  ACT  %d  %d  %h\n",channel,bg,ba,row);
-//$fwrite(fh1, "128 %d  RD   %d  %d  %h\n",channel,bg,ba,col);
-//end
-
 end
 
 
-
-`endif
+cpu_clk_count=cpu_clk_count+1;
+end //82
+//else disable my_always_block;
 end
-//$fclose(fh);
-//$fclose(fh1);
+//end
 ////////////////////////////////////////////////////////////
 
+//////////////////INITIAL BLOCK FOR PARSING/////////////////
+initial begin
+cpu_clk_count=0;
+enable=1'b1;
+
+
+//$fclose(fh);
+//$fclose(fh1);
+//#2000 $finish;
+end  //line 5
 
 //end
 
 
-endmodule 
+endmodule
+
+
+

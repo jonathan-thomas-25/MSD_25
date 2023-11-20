66ee9a854cb19660ab9625dacf5c2fa35e834027
jonathan-thomas-25 <jonthom@pdx.edu>
2023-11-20 10:53:29 -0800

    Added the queu logic

--- mc_scheduler.sv
+++ mc_scheduler.sv
@@ -1,25 +1,27 @@
-module nov17_mc;
+module mc_scheduler;
 
 `timescale 1ps/1ps
 
-//////////////////////////VARIABLE DECLARATIONS//////////////
-
-
-//PARSING SIGNALS
-
-string filename;
-string filename1;
+bit [0:0] cpu_clock;
+bit [0:0] dimm_clock;
+always #104 cpu_clock=~cpu_clock;
+always #208 dimm_clock=~dimm_clock;
+bit [0:0] p;
 int fh;
 int fh1;
-string line;
-int c=0;
-
-string clock_cycle;
-string core;
-string request_type;
-string address;
+bit [0:0] file_done;
+string main_q[$:16];
+string filename;
+string filename1;
+longint cpu_clock_count;
+longint dimm_clock_count;
+string transaction;
 string parts[4];
-
+longint clock_cycle_int;
+string clock_cycle;
+bit [0:0] enable;
+string line;
+int c;
 logic [33:0] address_hex;
 logic [1:0] Byte_Select;
 logic [3:0] col_low;
@@ -28,22 +30,116 @@ logic [9:0] col;
 logic [1:0] ba;
 logic [2:0] bg;
 logic [15:0] row;
-logic channel;
-int clock_cycle_output;
-string command;
-int clock_cycle_int;
-
-//HARDWARE SIGNALS
-string main_q[$:15];
-bit [0:0] cpu_clk;
-always #104 cpu_clk=~cpu_clk;
-longint cpu_clk_count=0;
 bit [0:0] debug_check;
-bit [0:0] enable;
-//////////////////////ALWAYS LOGIC//////////////////////////
+string request_type;
+logic channel;
+string core;
+string address;
 
-always @(posedge cpu_clk)begin :my_always_block
-if(cpu_clk_count==0)begin
+
+always @(posedge cpu_clock)begin
+
+if(file_done==1'b1)begin
+if(enable==1'b1)begin
+if(!$feof(fh) && $fgets(line,fh))begin                         
+
+
+$sscanf(line,"%s %s %s %s",parts[0],parts[1],parts[2],parts[3]);
+
+clock_cycle=parts[0];
+core=parts[1];
+request_type=parts[2];
+address=parts[3];
+clock_cycle_int=clock_cycle.atoi();
+enable=1'b0;
+
+address_hex=address.atohex();
+//$display("The address is hex value is %h\n",address_hex);
+
+channel=address_hex[6];
+Byte_Select=address_hex[1:0];
+col_low=address_hex[5:2];
+col_high=address_hex[17:12];
+col={col_high,col_low};
+ba=address_hex[11:10];
+bg=address_hex[9:7];
+row=address_hex[33:18];
+enable=1'b0;
+clock_cycle_int=clock_cycle.atoi();
+
+end
+
+//else $finish;
+
+end
+end
+end
+
+always @(posedge cpu_clock)begin
+cpu_clock_count=cpu_clock_count+1;
+
+if(cpu_clock_count==clock_cycle_int)begin
+
+if(debug_check==1'b1)begin 
+
+
+if(request_type=="0" || request_type=="2")begin
+$display("%0d %s %s  %d  PRE  %d  %d\n",cpu_clock_count,core,request_type,channel,bg,ba);
+end
+
+if(request_type=="1")begin
+$display("%0d %s %s %d  PRE  %d  %d\n",cpu_clock_count,core,request_type,channel,bg,ba);
+end
+
+c=c+1;
+if(c>=5)begin
+$finish;
+end
+
+end//if(debug_check==1'b1)
+
+//$fwrite(fh1,"Item entered in the queue at time %t\n",$time);
+main_q.push_back(clock_cycle);
+enable=1'b1;
+
+end
+
+end
+
+
+
+always @(posedge dimm_clock)begin
+
+if(main_q.size()>0)begin
+transaction=main_q.pop_front();
+
+if(debug_check==1'b0)begin
+
+if(request_type=="0" || request_type=="2")begin
+$fwrite(fh1,"%d %s %s  %d  PRE  %d  %d\n",cpu_clock_count,core,request_type,channel,bg,ba);
+end
+
+if(request_type=="1")begin
+$fwrite(fh1,"%d %s %s  %d  PRE  %d  %d\n",cpu_clock_count,core,request_type,channel,bg,ba);
+end
+
+
+end
+//$fwrite(fh1,"[TRANSACTION]The current clock cycle is %s and the time is %t\n",transaction,$time);
+end
+
+end
+
+
+
+
+initial begin
+
+cpu_clock=1'b0;
+dimm_clock=1'b0;
+enable=1'b1;
+file_done=1'b0;
+c=0;
 
 if($value$plusargs("INPUT_FILE=%s", filename))begin
 
@@ -76,66 +172,10 @@ debug_check=1'b1;
 debug_check=1'b0;
 `endif
 
-end
 
-if(!$feof(fh) && $fgets(line,fh))begin
+file_done=1'b1;
 
-
-$sscanf(line,"%s %s %s %s",parts[0],parts[1],parts[2],parts[3]);
-
-clock_cycle=parts[0];
-core=parts[1];
-request_type=parts[2];
-address=parts[3];
-
-if(debug_check==1'b1)begin 
-
-$display("###################### TRANSACTIONS ##############################\n");
-$display("clock cycle=%s\n",clock_cycle);
-$display("core number = %s\n",core);
-$display("request type=%s\n",request_type);
-$display("address=%s\n",address);
-
-c=c+1;
-if(c>=5)begin
-break;
-end
 
 end
-
-if(debug_check==1'b0)begin
-
-$fwrite(fh1,"###################### TRANSACTIONS ##############################\n");
-$fwrite(fh1,"clock cycle=%s\n",clock_cycle);
-$fwrite(fh1,"core number = %s\n",core);
-$fwrite(fh1,"request type=%s\n",request_type);
-$fwrite(fh1,"address=%s\n",address);
-
-end
-
-
-cpu_clk_count=cpu_clk_count+1;
-end //82
-//else disable my_always_block;
-end
-//end
-////////////////////////////////////////////////////////////
-
-//////////////////INITIAL BLOCK FOR PARSING/////////////////
-initial begin
-cpu_clk_count=0;
-enable=1'b1;
-
-
-//$fclose(fh);
-//$fclose(fh1);
-//#2000 $finish;
-end  //line 5
-
-//end
-
 
 endmodule
-
-
-
